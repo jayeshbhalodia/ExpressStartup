4afca6a0c85e4df5c815e255a9c9050457b0a669
JS Bhalodia <jayeshbhalodia@ymail.com>
2016-03-24 16:02:06 +0530

    Clean Desk

--- public/angular/main.ts
+++ public/angular/main.ts
@@ -1,10 +1,10 @@
 import {bootstrap}    from "angular2/platform/browser";
-import {AppComponent} from "./components/app.component";
 import {ROUTER_PROVIDERS, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 
+import {AppComponent} from "./routes";
 
-//
-//
+
+
 bootstrap(AppComponent, [
     ROUTER_PROVIDERS
 ]);
--- public/angular/routes.ts
+++ public/angular/routes.ts
@@ -1,7 +1,20 @@
-// @Component({ ... })
-// @RouteConfig([
-//   {path:'/crisis-center', name: 'CrisisCenter', component: CrisisListComponent},
-//   {path:'/heroes',        name: 'Heroes',       component: HeroListComponent},
-//   {path:'/hero/:id',      name: 'HeroDetail',   component: HeroDetailComponent}
-// ])
-// export class AppComponent { }
+import {Component} from 'angular2/core';
+import {ROUTER_PROVIDERS, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+
+import {AboutUs, Home} from "./components/home";
+
+
+@Component({
+    selector: 'start-engine',
+    templateUrl: '/angular/views/layout.html',
+    directives: [ROUTER_DIRECTIVES]
+})
+
+@RouteConfig([
+  {path:'/', name: 'Home', component: Home},
+  {path:'/about-us', name: 'AboutUS', component: AboutUs},
+])
+
+export class AppComponent {
+
+}
